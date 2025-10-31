2b3ea9866ce84c3b77bb50cb70ef0f74cef22490
JT <jstorbit@gmail.com>
2025-10-30 19:16:45 -0600

    Add concatenated combo in codeA

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const code1 = 7 + 3;
 const code2 = (100 - 20) / 2;
 const code3 = 30 + (19 % 10); 
 const message = "The vault has been secured. The combination is: ";
+const codeA = code1 + "-" + code2 + "-" + code3;
 
 
 console.log(code1, code2, code3);
